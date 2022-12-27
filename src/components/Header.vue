240d7c5167be2fb872c205bb42559d38c4f3b1eb
Mac-25 <mac-25@Mac-25s-MacBook-Pro.local>
2022-12-27 21:11:45 +0500

    Added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+<!--    <h1>Task Tracker</h1>-->
+    <h1>{{title}}</h1>
+  </header>
+</template>
+
+
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello World'
+    }
+  }
+}
+</script>
+
+
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
